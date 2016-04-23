ebf290df1c329d5caa94eea2a6578a13eb9bf3ce
Gur Dotan <gurdotan@gmail.com>
2016-04-23 23:26:44 +0300

    Enhance skeleton, add basic socket communication

--- app.js
+++ app.js
@@ -1,9 +1,55 @@
-var express  = require('express');
-var app      = express(); 								
-var port  	 = 3000; 				
+'use strict';
+
+// define globals
+var express      = require('express'),
+    io           = require('socket.io'),
+    http         = require('http'),
+    app          = express(),
+    server       = http.createServer(app),
+    io           = io.listen(server),
+    path         = require('path'),
+    // favicon      = require('static-favicon'),
+    // logger       = require('morgan'),
+    // cookieParser = require('cookie-parser'),
+    // bodyParser   = require('body-parser'),
+    port         = 3000;
 
 app.use(express.static(__dirname + '/client')); 		// statics
 require('./server/routes.js')(app);						// routes
 
-app.listen(port);										// let the games begin!
+// set up our socket server
+require('./server/sockets')(io);
+
+
+// optional - set socket.io logging level
+io.set('log level', 1000);
+
+/*
+ // view engine setup (for later)
+ app.set('views', path.join(__dirname, 'views'));
+ app.set('view engine', 'jade');
+
+ // middleware settings
+ app.use(favicon());
+ app.use(logger('dev'));
+ app.use(bodyParser.json());
+ app.use(bodyParser.urlencoded());
+ app.use(cookieParser());
+ app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+ */
+
+
+
+// start the server
+server.listen(port);
 console.log("Web server listening on port " + port);
+
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+
+//module.exports = app;
--- client/js/app.js
+++ client/js/app.js
@@ -1,22 +1,75 @@
 // App
-var app = angular.module('app', []);
+var app = angular.module('app', [
+  'btford.socket-io'
+]);
 
 // Service to fetch some data..
-app.factory('dataServ', ['$http',function($http) {
-	return {
-		get : function() {
-			return $http.get('/data');
-		}
-	}
+app.factory('dataServ', ['$http', function($http) {
+  return {
+    get: function() {
+      return $http.get('/data');
+    }
+  }
 }]);
 
-// App controller
-app.controller('appController', ['$scope','dataServ', function($scope, Data) {
-	
-	$scope.funnyStuff = {question: '', answer: ''};
+app.factory('chatSocket', function (socketFactory) {
+  var socket = socketFactory();
+  socket.forward('broadcast');
+  return socket;
+});
 
-	Data.get()
-		.success(function(resp) {
-			$scope.funnyStuff = resp;
-		});
+app.value('messageFormatter', function(date, nick, message) {
+  return date.toLocaleTimeString() + ' - ' +
+    nick + ' - ' +
+    message + '\n';
+
+});
+
+
+// App controller
+app.controller('appController', ['$scope', 'dataServ', 'chatSocket', '$log', 'messageFormatter', function($scope, Data, chatSocket, $log, messageFormatter) {
+
+  $scope.funnyStuff = {question: '', answer: ''};
+
+  Data.get()
+    .success(function(resp) {
+      $scope.funnyStuff = resp;
+    });
+
+
+  var number = Math.random();
+  var nickName = 'Gur' + number;
+  $scope.message = 'Hi there ' + number;
+  $scope.nickName = nickName;
+  $scope.messageLog = 'Ready to chat!';
+  $scope.sendMessage = function() {
+    var match = $scope.message.match('^\/nick (.*)');
+
+    if (angular.isDefined(match) && angular.isArray(match) && match.length === 2) {
+      var oldNick = nickName;
+      nickName = match[1];
+      $scope.message = '';
+      $scope.messageLog = messageFormatter(new Date(),
+          nickName, 'nickname changed - from ' +
+          oldNick + ' to ' + nickName + '!') + $scope.messageLog;
+      $scope.nickName = nickName;
+    }
+
+    $log.debug('sending message', $scope.message);
+    chatSocket.emit('message', nickName, $scope.message);
+    // $scope.message = '';
+  };
+
+  $scope.$on('socket:broadcast', function(event, data) {
+    $log.debug('got a message', event.name);
+    if (!data.payload) {
+      $log.error('invalid message', 'event', event, 'data', JSON.stringify(data));
+      return;
+    }
+    $log.debug(data);
+    $scope.$apply(function() {
+      $scope.messageLog = $scope.messageLog + messageFormatter(new Date(), data.source, data.payload);
+    });
+  });
+
 }]);
